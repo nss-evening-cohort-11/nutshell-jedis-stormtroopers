688a5f015a1817bb913a58ac9cc8e3004627fabf
Aaron Lain <aaron.j.lain@gmail.com>
2020-04-15 13:28:10 -0500

    added moving graph code + plotly

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,46 @@
+import plotly from 'plotly.js/dist/plotly';
 import '../styles/main.scss';
+// import utils from './helpers/utils';
+
+const randomChart = () => {
+  const getRandValue = () => Math.ceil(Math.random() * 100);
+
+  plotly.plot('chart', [{
+    y: [getRandValue()],
+    type: 'line',
+  }], {
+    colorway: ['#cd7eaf'],
+    xaxis: {
+      title: {
+        text: 'Number of Ratings',
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Customer Ratings',
+      },
+    },
+  });
+
+  let count = 0;
+
+  setInterval(() => {
+    plotly.extendTraces('chart', { y: [[getRandValue()]] }, [0]);
+    count += 1;
+    if (count >= 300) {
+      plotly.relayout('chart', {
+        xaxis: {
+          range: [count - 300, count],
+        },
+      });
+    }
+  }, 40);
+};
+
+const init = () => {
+  randomChart();
+};
+
+init();
 
 console.error('hi');
