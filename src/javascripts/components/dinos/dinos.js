a3647ecb84536a1147d2200b8c1361ef963ad5d8
Aaron Lain <aaron.j.lain@gmail.com>
2020-04-15 22:02:31 -0500

    dinos print

--- src/javascripts/components/dinos/dinos.js
+++ src/javascripts/components/dinos/dinos.js
@@ -2,29 +2,28 @@ import 'firebase/auth';
 import dinoData from '../../helpers/data/dinoData';
 import utils from '../../helpers/utils';
 
-
-const printHungryDino = (dinoName, bool) => {
-  let domString = '';
-  domString += bool ? `${dinoName} is hungry!` : `${dinoName} is fine.`;
-  return domString;
-};
-
 const printDinos = (dino) => {
   let domString = '';
-  domString += '<div class="card" style="width: 18rem;">';
+  domString += '<div class="card col-3" style="width: 18rem;">';
   domString += `<h3>${dino.name}</h3>`;
   domString += `<img class="card-img-top" src="${dino.photoUrl}" alt="Card image cap">`;
   domString += '<div class="card-body">';
-  domString += printHungryDino(`${dino.name}`, dino.isHungry);
+  domString += dino.isHungry ? `${dino.name} is hungry!` : `${dino.name} is fine.`;
   domString += '</div>';
   domString += '</div>';
 
-  utils.printToDom('dino-dashboard', domString);
+  return domString;
 };
 
 const printDinosDashboard = () => {
   dinoData.getDinos()
-    .then((dinos) => dinos.forEach((dino) => printDinos(dino)))
+    .then((dinos) => {
+      let domString = '';
+      dinos.forEach((dino) => {
+        if (dino) domString += printDinos(dino);
+      });
+      utils.printToDom('dino-dashboard', domString);
+    })
     .catch((err) => console.error('printDinosDashboard broke', err));
 };
 
