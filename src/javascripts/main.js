0e33c7b84b25de1592562969b33809a96ff1bced
Steven Zelenak <stevenmzelenak@gmail.com>
2020-04-15 21:37:57 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,17 +1,18 @@
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 
-import navbarComponent from './components/navbar/navbar';
-import authData from './helpers/data/authData';
 import 'bootstrap';
 import '../styles/main.scss';
+
+import navbarComponent from './components/navbar/navbar';
+import authData from './helpers/data/authData';
 import auth from './components/auth/auth';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
-  $('body').on('click', '.nav-icon', navbarComponent.navbarEvents);
   auth.loginButton();
+  $('body').on('click', '.nav-icon', navbarComponent.navbarEvents);
 };
 
 init();
