7f2d14b2e38d3d5cb8741d8ded381cde8e9c115a
Steven Zelenak <stevenmzelenak@gmail.com>
2020-04-15 21:21:26 -0500

    wip

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,14 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const loginButton = '#google-auth';
+const loginButton = $('#google-auth');
+const logoutButton = $('#logout-btn');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       loginButton.addClass('hide');
+      logoutButton.removeClass('hide');
     } else {
       loginButton.removeClass('hide');
+      logoutButton.addClass('hide');
     }
   });
 };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,15 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
 import navbarComponent from './components/navbar/navbar';
+import authData from './helpers/data/authData';
 import 'bootstrap';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
   $('body').on('click', '.nav-icon', navbarComponent.navbarEvents);
   auth.loginButton();
 };
