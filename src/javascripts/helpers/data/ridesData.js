f689515c1c2759182ec85d04cc2049aea5c0c823
John Fleming <johndeclanfleming@gmail.com>
2020-04-18 09:59:36 -0500

    axios data requests written

---
+++ src/javascripts/helpers/data/ridesData.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getRides = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/rides.json`)
+    .then((response) => {
+      const ridesResponse = response.data;
+      const rides = [];
+      if (ridesResponse) {
+        Object.keys(ridesResponse).forEach((rideId) => {
+          ridesResponse[rideId].id = rideId;
+          rides.push(ridesResponse[rideId]);
+        });
+      }
+      resolve(rides);
+    })
+    .catch((err) => reject(err));
+});
+
+const deleteRide = (rideId) => axios.delete(`${baseUrl}/rides/${rideId}.json`);
+
+const addRide = (newRide) => axios.post(`${baseUrl}/rides/${newRide}.json`);
+
+const updateRide = (rideId, modifiedRide) => axios.put(`${baseUrl}/rides/${rideId}.json`, modifiedRide);
+
+const getSingleRide = (rideId) => axios.get(`${baseUrl}/rides/${rideId}.json`);
+
+export default {
+  getRides,
+  deleteRide,
+  addRide,
+  updateRide,
+  getSingleRide,
+};
