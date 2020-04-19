7fabaec116f97462203350cbbb8c1109c0b4364d
Steven Zelenak <stevenmzelenak@gmail.com>
2020-04-18 22:27:59 -0500

    updated

--- src/javascripts/components/equipment/equipment.js
+++ src/javascripts/components/equipment/equipment.js
@@ -21,15 +21,15 @@ const buildEquipmentForm = () => {
   domString += '<form class="px-2 py-2 bg-light mb-3 col-12 text-center justify-content-center">';
   domString += '<div class="form-group">';
   domString += '<label for="equipment-name">Name</label>';
-  domString += '<input type="text" class="form-control" id="equipment-name" aria-describedby="emailHelp" placeholder="Jeep">';
+  domString += '<input type="text" class="form-control" id="equipment-name" aria-describedby="emailHelp" placeholder="Enter equipment name">';
   domString += '</div>';
   domString += '<div class="form-group">';
   domString += '<label for="equipment-description">Description</label>';
-  domString += '<input type="text" class="form-control" id="equipment-description" placeholder="A Jeep painted in cool colors">';
+  domString += '<input type="text" class="form-control" id="equipment-description" placeholder="Enter equipment description">';
   domString += '</div>';
   domString += '<div class="form-group">';
   domString += '<label for="equipment-image">Image-Url</label>';
-  domString += '<input type="text" class="form-control" id="equipment-image" placeholder="image-Url">';
+  domString += '<input type="text" class="form-control" id="equipment-image" placeholder="Enter equipment imgae Url">';
   domString += '</div>';
   domString += '<button type="submit" id="add-equipment" class="btn btn-primary">Submit</button>';
   domString += '</form>';
