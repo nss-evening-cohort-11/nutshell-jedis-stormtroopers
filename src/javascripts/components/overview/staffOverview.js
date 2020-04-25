6c5780f1ae9395ff1267cb242c3a0fa31b0a9aed
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-04-25 14:11:05 -0500

    wip

--- src/javascripts/components/overview/staffOverview.js
+++ src/javascripts/components/overview/staffOverview.js
@@ -1,38 +1,28 @@
 import utils from '../../helpers/utils';
 import chaosMonkeyData from '../../helpers/data/chaosMonkeyData';
+import staffData from '../../helpers/data/staffData';
 
 const getMissingStaffList = () => {
   chaosMonkeyData.getChaosEventsByType('kidnap')
     .then((eventsArray) => {
       let domString = '';
       eventsArray.forEach((event) => {
-        domString += `<p>Kidnapping at ${event.timestamp}</p>`;
-      });
-      utils.printToDom('staff-overview-container', domString);
+        domString += '<p>';
+        const staffId = event.affectectEntityId;
+        staffData.getSingleStaffMemeber(staffId)
+        .then(() => domString += `${staffMember.name}`);
+        domString += '(presumed dead) - kidnapped/missing ${event.timestamp}</p>';
+      })
+      utils.printToDom('staff-overview-container', domString),
     })
     .catch((err) => console.error('problem with getMissingStaffList', err));
 };
-
-// const getMissingStaffCount = () => {
-//   staffData.getStaffs()
-//     .then((resp) => {
-//       const missing = resp.filter((x) => x.isKidnapped);
-//       let domString = '';
-//       domString += '<p>Missing Staff Count:</p>';
-//       domString += `<div id="missing-staff-count" class="text-center mt-5">${missing.length}</div>`;
-//       utils.printToDom('missing-staff', domString);
-//     })
-//     .catch((err) => console.error('could not get missing staff', err));
-// };
 
 const buildStaffOverview = () => {
   let domString = '';
   domString += '<div id="staff-overview-container" class="text-center">';
   domString += getMissingStaffList();
   domString += '</div>';
-  // domString += '<div id="missing-staff">';
-  // domString += getMissingStaffCount();
-  // domString += '</div>';
   return domString;
 };
 
