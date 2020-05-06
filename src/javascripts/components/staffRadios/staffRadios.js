6c16d54a4f25a7be26dfd16392032bf16cc13cb5
David Everett <davideverett1989@gmail.com>
2020-05-06 17:49:21 -0500

    Nope this is the last one.

--- src/javascripts/components/staffRadios/staffRadios.js
+++ src/javascripts/components/staffRadios/staffRadios.js
@@ -3,7 +3,6 @@ import smash from '../../helpers/data/smash';
 
 const buildStaffRadios = (shiftId, vendorId) => {
   smash.getAllStaffWithJobs().then((staffMembers) => {
-    console.log(vendorId);
     let domString = '';
     domString += '<div>';
     domString += '  <div class="d-flex flex-row justify-content-between align-items-center card-header text-center">';
